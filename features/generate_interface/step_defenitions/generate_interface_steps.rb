d1d7d406b771101f02c89776ba3709ae428ac237
Jasper Kennis <tutorialheaven@gmail.com>
2011-07-04 15:13:37 +0200

    Copied new suggested step defenitions into defenition file.

---
+++ features/generate_interface/step_defenitions/generate_interface_steps.rb
@@ -0,0 +1,7 @@
+When /^I upload the TouchOSC file$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should see a message confirming the upload was successfully transfered$/ do
+  pending # express the regexp above with the code you wish you had
+end
