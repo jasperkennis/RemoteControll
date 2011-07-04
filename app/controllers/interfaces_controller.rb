25996be291f0d5c3e862e8bce2c0f178f6881eaa
Jasper Kennis <tutorialheaven@gmail.com>
2011-07-04 16:13:50 +0200

    Created new def in the interface controller.

--- app/controllers/interfaces_controller.rb
+++ app/controllers/interfaces_controller.rb
@@ -1,2 +1,10 @@
 class InterfacesController < ApplicationController
+  def new
+    @interface = Interface.new
+    
+    respond_to do |format|
+      format.html
+      format.xml  { render :xml => @interface }
+    end
+  end
 end
