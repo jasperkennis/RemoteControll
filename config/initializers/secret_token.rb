e4a7738a2816a9e624c31d3d53ac9db62a6f49c9
Jasper Kennis <tutorialheaven@gmail.com>
2011-07-04 14:06:13 +0200

    It's a bare project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Remotecontroll::Application.config.secret_token = '8ab56c06a1a41cf8a7429de765ee8b74059c661ffecf97f4206c4cb3dd7362fde688272ee7d5f18218608b7f03e02011b9eaf289336fdb61b63cf44f198aad88'
