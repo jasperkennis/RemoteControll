d78a2630ed599f45077e990abded2c0f02999461
Jasper Kennis <tutorialheaven@gmail.com>
2011-07-05 22:22:59 +0200

    Redded all specs for interface names.

--- spec/controllers/interface_controller_spec.rb
+++ spec/controllers/interface_controller_spec.rb
@@ -15,11 +15,26 @@ describe InterfacesController do
     no_name_interface.should_not be_valid
   end
   
-  it "should reject long names"
+  it "should reject names that are too short" do
+    too_short_name = "a" * 2
+    too_short_name_interface = Interface.new(@attr.merge(:name => too_short_name))
+    too_short_name_interface.should_not be_valid
+  end
   
-  it "should reject short names"
+  it "should reject names that are too long" do
+    too_long_name = "a" * 41
+    too_long_name_interface = Interface.new(@attr.merge(:name => too_long_name))
+    too_long_name_interface.should_not be_valid
+  end
+  
+  it "should reject duplicate names" do
+    Interface.create(@attr)
+    interface_with_duplicate_name = Interface.new(@attr)
+    interface_with_duplicate_name.should_not be_valid
+  end
   
   it "should require a json string"
   
-  it "should reject duplicate names"
+  it "should require a non json string"
+  
 end
