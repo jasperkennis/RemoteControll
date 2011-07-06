8903794bb9f6444620834aee17c74b1addb11158
Jasper Kennis <tutorialheaven@gmail.com>
2011-07-06 15:38:02 +0200

    Wrote the initial logics for converting .touchosc files to json strings.

--- app/controllers/interfaces_controller.rb
+++ app/controllers/interfaces_controller.rb
@@ -1,3 +1,7 @@
+require 'zip/zip'
+
+
+
 class InterfacesController < ApplicationController
 	def new
 		@interface = Interface.new
@@ -9,7 +13,26 @@ class InterfacesController < ApplicationController
 	end
 
 	def create
-		@interface = Interface.new(params[:interface])
+
+    # Write the received file and save it to tmp/uploads
+		name =  params[:interface]["json"].original_filename.gsub('touchosc','zip')
+		directory = "tmp/uploads"
+		path = File.join(directory, name)
+		File.open(path, "wb") { |f| f.write(params[:interface]["json"].read) }
+
+    #Unzip the file to its own name, minus .zip
+		unzip_file(path,path.gsub('.zip',''))
+    
+    # Convert all children to json strings, and push them into the master json string
+    @json = '{"interfaces":['
+		Dir[File.join(path.gsub('.zip',''), "*.xml")].each{ |l|
+			@file = get_file_as_string(l)
+			@json << Hash.from_xml(@file).to_json.gsub(/\n/, '') << ','
+		}
+		@json = @json.chop
+		@json << ']}'
+
+    @interface = Interface.new(params[:interface].merge(:json => @json))
 
 		respond_to do |format|
 			if @interface.save
@@ -24,40 +47,27 @@ class InterfacesController < ApplicationController
 			end
 		end
 	end
-	
+
 	def show
-	  
-	end
-end
 
-
-
-class UnZipThemAll
-	attr_accessor :zip_file_path, :to_folder_path
-	def initialize( zip_file_path, to_folder_path )
-		@zip_file_path = zip_file_path
-		@to_folder_path = to_folder_path
 	end
 
-	def unzip
-		if File.exists?( self.zip_file_path ) == false
-			puts "Zip file #{self.zip_file_path} does not exist!"
-		return
+	def unzip_file (file, destination)
+		Zip::ZipFile.open(file) { |zip_file|
+			zip_file.each { |f|
+				f_path=File.join(destination, f.name)
+				FileUtils.mkdir_p(File.dirname(f_path))
+				zip_file.extract(f, f_path) unless File.exist?(f_path)
+			}
+		}
+	end
+
+	def get_file_as_string(filename)
+		data = ''
+		f = File.open(filename, "r")
+		f.each_line do |line|
+			data += line
 		end
-
-		if File.exists?( self.to_folder_path ) == false
-			FileUtils.mkdir( self.to_folder_path )
-		end
-
-		zip_file = Zip::ZipFile.open( self.zip_file_path )
-		Zip::ZipFile.foreach( self.zip_file_path ) do | entry |
-			file_path = File.join( self.to_folder_path, entry.to_s )
-			if File.exists?( file_path )
-				FileUtils.rm( file_path )
-			end
-
-			zip_file.extract( entry, file_path )
-		end
-
+		return data
 	end
 end
