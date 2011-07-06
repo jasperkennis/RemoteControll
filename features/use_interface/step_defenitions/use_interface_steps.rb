ec8db9498a715953b02391f7be811a89c2cd1663
Jasper Kennis <tutorialheaven@gmail.com>
2011-07-06 23:35:07 +0200

    Redefined Use Interface feature.

---
+++ features/use_interface/step_defenitions/use_interface_steps.rb
@@ -0,0 +1,7 @@
+Then /^I should see a green button$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should see a red button$/ do
+  pending # express the regexp above with the code you wish you had
+end
