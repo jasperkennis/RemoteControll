34804137549ec287d20d0f93365b1ab783f9b9c8
Jasper Kennis <tutorialheaven@gmail.com>
2011-07-06 11:21:25 +0200

    Altered the interface specs a bit, and redded them.

--- spec/controllers/interface_controller_spec.rb
+++ spec/controllers/interface_controller_spec.rb
@@ -3,7 +3,32 @@ require 'spec_helper'
 describe InterfacesController do
 
   before(:each) do
-    @attr = { :name => "Example User", :json => '{"somestring":[]}' }
+    @attr = { :name => "Example User", :json => '{"widget": {
+    "debug": "on",
+    "window": {
+        "title": "Sample Konfabulator Widget",
+        "name": "main_window",
+        "width": 500,
+        "height": 500
+    },
+    "image": { 
+        "src": "Images/Sun.png",
+        "name": "sun1",
+        "hOffset": 250,
+        "vOffset": 250,
+        "alignment": "center"
+    },
+    "text": {
+        "data": "Click Here",
+        "size": 36,
+        "style": "bold",
+        "name": "text1",
+        "hOffset": 250,
+        "vOffset": 100,
+        "alignment": "center",
+        "onMouseUp": "sun1.opacity = (sun1.opacity / 100) * 90;"
+    }
+}}' }
   end
 
   it "should create a new instance given valid attributes" do
@@ -28,14 +53,22 @@ describe InterfacesController do
   end
   
   it "should reject duplicate names" do
-    Interface.create(@attr)
+    Interface.create!(@attr)
     interface_with_duplicate_name = Interface.new(@attr)
     interface_with_duplicate_name.should_not be_valid
   end
   
-  it "should require a json valid string"
+  it "should reject an interface without a json string" do
+    empty_json_string = "2" * 10
+    empty_json_string_interface = Interface.new(@attr.merge(:json => empty_json_string))
+    empty_json_string_interface.should_not be_valid
+  end
   
-  it "should reject a non json string"
+  it "should reject an interface with an invalid json string" do
+    invalid_json_string = "2" * 10
+    invalid_json_string_interface = Interface.new(@attr.merge(:json => invalid_json_string))
+    invalid_json_string_interface.should_not be_valid
+  end
   
   
 end
