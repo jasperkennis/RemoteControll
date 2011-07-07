8adfd3c8f0cca7dc905f76f4f9440dcf8aa557f9
Jasper Kennis <tutorialheaven@gmail.com>
2011-07-07 12:18:15 +0200

    Created first bit of prototype logics that are to create the interface.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,7 +1,35 @@
 document.observe("dom:loaded", function(){
 	
 	// If we're looking at an interface
-	if($('interface') != undefined){
-		console.log($('interface'));
+	if($('interfaces') != undefined){
+		
+		/*
+		 * If an interfaces JSON string is defined, the original screen
+		 * contents are removed, and the JSON string is parsed into an
+		 * object, which is than used to create that interface.
+		 */
+		
+		if($('interfaces').innerText.isJSON()){
+			
+			// Parse the JSON string into an object before clearing body
+			var interfaces = $('interfaces').innerText.evalJSON().interfaces;
+			
+			// Remove all elements from body
+			$$('body')[0].childElements().each(function(e){
+				e.remove();
+			});
+			
+			
+			
+		} else {
+			alert("This interface is broken. Unfortunately, you can't use this application right now.");
+			log($('interfaces'));
+		}
 	}
 });
+
+function log(text){
+	if(console){
+		console.log(text);
+	}
+}
