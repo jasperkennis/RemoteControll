2c9d60e5033c4185fe17d87bfd90ac8c5b95e9a7
Jasper Kennis <tutorialheaven@gmail.com>
2011-08-10 14:47:52 +0200

    Moved the processing of interfaces into a separate function

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -16,41 +16,66 @@ var InterfaceController = {
 			
 			// Parse the JSON string into an object before clearing body
 			var interfaces = $('interfaces').innerText.evalJSON().interfaces;
-			log(interfaces);
 			
 			// Remove all elements from body
 			$$('body')[0].childElements().each(function(e){
 				e.remove();
 			});
 			
-			// Iterate over and build all the layouts
-			interfaces.each(function(layout){
-				// Build all elements:
-				layout.layout.tabpage.control.each(function(widget){
-					self.buildWidget(
-						'led',
-						{ w: widget.w, h: widget.h },
-						{ x: widget.x, y: widget.y },
-						widget.color,
-						widget.name,
-						false);
-					log(widget);
-				})
-				log(layout);
-			});
-			
+			// See if iteration is needed:
+			if(interfaces.length > 1) {
+				// Iterate over and build all the layouts
+				interfaces.each(function(layout){
+					self.processInterface(layout);
+				});
+			} else {
+				var layout = interfaces;
+				self.processInterface(layout);
+			}
+
 		} else {
 			alert("This interface is broken. Unfortunately, you can't use this application right now.");
 			log($('interfaces'));
 		}
 	},
 	
+	
+	
+	/**
+	 * processInterface
+	 * Iterates over all widgets in an interface and calls
+	 * buidWidget on them.
+	 * 
+	 * @param _interface The interface to process
+	 */
+	processInterface : function(_interface){
+		if(!self){ var self = this; }
+		
+		log(_interface[0].layout);
+		// Build all elements:
+		_interface[0].layout.tabpage.control.each(function(widget){
+			self.buildWidget(
+				'led',
+				{ w: widget.w, h: widget.h },
+				{ x: widget.x, y: widget.y },
+				widget.color,
+				widget.name,
+				false);
+			//log(widget);
+		});
+	},
+	
+	
+	
+	/**
+	 * buildWidget
+	 * Takes a number of arguments and constructs the
+	 * correct widget.
+	 */
 	buildWidget: function(type,size,position,color,name,relative){
 		
 		// Define self to prevent conflicts with frameworks
 		if(!self){ var self = this; }
-		
-		
 		
 		/*
 		 * Create a widget that responds to user input. Uses the
