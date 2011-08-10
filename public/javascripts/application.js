e06dc0a45051934d290c5aa4d6ce900f33b6e074
Jasper Kennis <tutorialheaven@gmail.com>
2011-08-10 11:09:47 +0200

    Wrote the first part of the logics for the client js.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,7 +1,10 @@
-document.observe("dom:loaded", function(){
-	
-	// If we're looking at an interface
-	if($('interfaces') != undefined){
+var InterfaceController = {
+	initInterface: function(){
+		
+		// Define self to prevent conflicts with frameworks
+		if(!self){ var self = this; }
+		
+		
 		
 		/*
 		 * If an interfaces JSON string is defined, the original screen
@@ -13,23 +16,66 @@ document.observe("dom:loaded", function(){
 			
 			// Parse the JSON string into an object before clearing body
 			var interfaces = $('interfaces').innerText.evalJSON().interfaces;
+			log(interfaces);
 			
 			// Remove all elements from body
 			$$('body')[0].childElements().each(function(e){
 				e.remove();
 			});
 			
-			
+			// Iterate over and build all the layouts
+			interfaces.each(function(layout){
+				// Build all elements:
+				layout.layout.tabpage.control.each(function(widget){
+					self.buildWidget(
+						'led',
+						{ w: widget.w, h: widget.h },
+						{ x: widget.x, y: widget.y },
+						widget.color,
+						widget.name,
+						false);
+					log(widget);
+				})
+				log(layout);
+			});
 			
 		} else {
 			alert("This interface is broken. Unfortunately, you can't use this application right now.");
 			log($('interfaces'));
 		}
+	},
+	
+	buildWidget: function(type,size,position,color,name,relative){
+		
+		// Define self to prevent conflicts with frameworks
+		if(!self){ var self = this; }
+		
+		
+		
+		/*
+		 * Create a widget that responds to user input. Uses the
+		 * paramaters destilled from the TouchOSC file.
+		 */
+		
+		switch(type){
+			case 'led':
+				break;
+			default:
+				break;
+		}
 	}
-});
+}
 
 function log(text){
 	if(console){
 		console.log(text);
 	}
 }
+
+document.observe("dom:loaded", function(){
+	
+	// If we're looking at an interface
+	if($('interfaces') != undefined){
+		InterfaceController.initInterface();
+	}
+});
