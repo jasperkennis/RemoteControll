043cc21f39cf52756a65fcdfa9cd8713a679cae3
Jasper Kennis <jasperkennis@creativetoken.nl>
2011-08-28 23:59:12 +0200

    Created a controller method that triggers a "led activated" message to spread over the socket network

--- app/controllers/screen_app_communication_controller.rb
+++ app/controllers/screen_app_communication_controller.rb
@@ -1,5 +1,14 @@
+require 'pusher'
+
+Pusher.app_id = '7972'
+Pusher.key = '7132d12d5d3ddf34b09e'
+Pusher.secret = 'd683b29be863f1fed7ed'
+
 class ScreenAppCommunicationController < ApplicationController
   def led
+    @message = 'Someone activated a led!'
+
+    Pusher['myscreen-input'].trigger('led-activated', @message)
   end
 
 end
