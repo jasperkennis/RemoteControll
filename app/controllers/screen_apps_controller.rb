690e399a8830b77ea5f76f6b845108d46c19bc49
Jasper Kennis <jasperkennis@creativetoken.nl>
2011-08-30 14:39:03 +0200

    Wrote method for create, index and new screen_apps.

--- app/controllers/screen_apps_controller.rb
+++ app/controllers/screen_apps_controller.rb
@@ -1,5 +1,19 @@
 class ScreenAppsController < ApplicationController
   def new
+    @screen_app = ScreenApp.new
+  end
+  
+  def index
+    @screen_apps = ScreenApp.all
   end
 
+  def create
+    @screen_app = ScreenApp.new(params[:screen_app])
+    
+    if @screen_app.save
+      render 'index'
+    else
+      render 'index'
+    end
+  end
 end
