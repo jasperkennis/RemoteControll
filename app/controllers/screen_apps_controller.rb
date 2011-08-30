dfe83b2ab1f2ae1100f16664f8a2d44ebe907362
Jasper Kennis <jasperkennis@creativetoken.nl>
2011-08-30 15:06:49 +0200

    Created edit and update actions for screen_apps

--- app/controllers/screen_apps_controller.rb
+++ app/controllers/screen_apps_controller.rb
@@ -6,6 +6,10 @@ class ScreenAppsController < ApplicationController
   def index
     @screen_apps = ScreenApp.all
   end
+  
+  def edit
+    @screen_app = ScreenApp.find(params[:id])
+  end
 
   def create
     @screen_app = ScreenApp.new(params[:screen_app])
@@ -15,5 +19,25 @@ class ScreenAppsController < ApplicationController
     else
       render 'index'
     end
+  end
+  
+  def update
+    @screen_app = ScreenApp.find(params[:id])
+   
+    respond_to do |format|
+      if @screen_app.update_attributes(params[:screen_app])
+        format.html { redirect_to(@screen_app,
+                      :notice => 'Post was successfully updated.') }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @screen_app.errors,
+                      :status => :unprocessable_entity }
+      end
+    end
+  end
+  
+  def show
+    @screen_app = ScreenApp.find(params[:id])
   end
 end
