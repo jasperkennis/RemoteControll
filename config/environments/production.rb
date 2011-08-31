228ea38b7220ba92bdc4d24cef566232994fad1e
Jasper Kennis <jasperkennis@creativetoken.nl>
2011-08-31 11:48:03 +0200

    Added root hosts to environment files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,7 @@ Remotecontroll::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  
+  #Required by devise:
+  config.action_mailer.default_url_options = { :host => 'http://blazing-dusk-603.heroku.com/' }
 end
