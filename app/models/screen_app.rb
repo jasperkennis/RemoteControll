487c8de79ef6a83b1b8318734cd5b52c4f51b356
Jasper Kennis <jasperkennis@creativetoken.nl>
2011-09-02 14:51:00 +0200

    Annotated models and extended some of their requirements.

--- app/models/screen_app.rb
+++ app/models/screen_app.rb
@@ -1,2 +1,22 @@
+# == Schema Information
+#
+# Table name: screen_apps
+#
+#  id          :integer         primary key
+#  name        :string(255)
+#  url         :string(255)
+#  logs_arrows :boolean
+#  created_at  :timestamp
+#  updated_at  :timestamp
+#
+
 class ScreenApp < ActiveRecord::Base
+  validates :name,  :presence => true,
+                    :length   => { :minimum => 3,
+                                   :maximum => 40 },
+                    :uniqueness => true
+  validates :url,   :presence => true,
+                    :uniqueness => true
+                    
+  validates_format_of :url, :with => URI::regexp(%w(http https))
 end
