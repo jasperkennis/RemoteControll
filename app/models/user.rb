62dbde5316de12b83ef74a6eaff6539e728a851a
Jasper Kennis <jasperkennis@creativetoken.nl>
2011-09-08 21:31:03 +0200

    Cleared certification scope

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ActiveRecord::Base
     data = access_token['extra']['user_hash']
     if user = User.find_by_email(data["email"])
       user
-    else # Create a user with a stub password. 
+    else # Create a user with a stub password.
       User.create(:email => data["email"], :name => data["name"], :password => Devise.friendly_token[0,20]) 
     end
   end
