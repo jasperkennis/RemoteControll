5b9d291cca82cd1c6bc33b5236b07a1bb8ceaf1b
Jasper Kennis <jasper@creativetoken.nl>
2011-11-28 14:01:59 +0100

    Moved script into separate file

--- db/schema.rb
+++ db/schema.rb
@@ -14,38 +14,38 @@
 ActiveRecord::Schema.define(:version => 20110908183700) do
 
   create_table "interfaces", :force => true do |t|
-    t.string   "name"
-    t.text     "json",          :limit => 255
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "screen_app_id"
+    t.string    "name"
+    t.text      "json"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
+    t.integer   "screen_app_id"
   end
 
   create_table "screen_apps", :force => true do |t|
-    t.string   "name"
-    t.string   "url"
-    t.boolean  "logs_arrows"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "user_id"
-    t.boolean  "register_arrows"
-    t.text     "description"
+    t.string    "name"
+    t.string    "url"
+    t.boolean   "logs_arrows"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
+    t.integer   "user_id"
+    t.boolean   "register_arrows"
+    t.text      "description"
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                                 :default => "", :null => false
-    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                         :default => 0
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "name"
+    t.string    "email",                                 :default => "", :null => false
+    t.string    "encrypted_password",     :limit => 128, :default => "", :null => false
+    t.string    "reset_password_token"
+    t.timestamp "reset_password_sent_at"
+    t.timestamp "remember_created_at"
+    t.integer   "sign_in_count",                         :default => 0
+    t.timestamp "current_sign_in_at"
+    t.timestamp "last_sign_in_at"
+    t.string    "current_sign_in_ip"
+    t.string    "last_sign_in_ip"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
+    t.string    "name"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
